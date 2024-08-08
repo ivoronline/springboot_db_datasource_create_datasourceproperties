bffad4b10320b2cc76883de2c490f249f0df3076
ivoronline <ivoronline@gmail.com>
2024-08-08 13:28:52 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_datasource_create_datasourceproperties/config/MyDatabaseConfig.java
+++ src/main/java/com/ivoronline/springboot_db_datasource_create_datasourceproperties/config/MyDatabaseConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class MyDatasourceConfig {
+public class MyDatabaseConfig {
 
   //=========================================================================================================
   // DATA SOURCE PROPERTIES 1
